c1990aedcc8fd634b132788d4df04f80c883c960
tyler <rowla070@morris.umn.edu>
2020-10-20 16:52:19 -0500

    All bats tests pass. Was missing 2 shutdownInputs, one in the client, one in the server. Added comments that we deemed necessary, removed comments that were unneccsary

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -16,14 +16,17 @@ public class EchoClient {
 	}
          
 	private void start() throws IOException {
+		//Create socket
 		Socket socket = new Socket("localhost", PORT_NUMBER);
 		InputStream input = socket.getInputStream();
 		OutputStream output = socket.getOutputStream();
-                
+
+		//create the client output thread and start it
 		WriteToServer write = new WriteToServer(output, socket);
 		Thread outputThread = new Thread(write);
 		outputThread.start();
 
+		//create the client input thread and start it
 		ReadFromServer read = new ReadFromServer(input, socket);
 		Thread inputThread = new Thread(read);
 		inputThread.start();
@@ -34,22 +37,24 @@ public class EchoClient {
 		int read;
 		Socket sock;
 
+		//Constructor
 		public ReadFromServer(InputStream input, Socket sock){
 			this.input = input;
 			this.sock = sock;
 		}
 
+		//Run method is where everything in the thread happens
 		@Override
 		public void run(){
 			try { // Keep reading until the user stops sending data
-
 				while ((read = input.read()) != -1){
 					System.out.write(read);
 					System.out.flush();
 				}
-				sock.shutdownInput(); //Close connection
+				sock.shutdownInput(); //Close connection to input
 
 			}
+			//Error Handling
 			catch (IOException ioe){
 				System.out.println("Caught an unexpected exception");
 			}
@@ -61,20 +66,24 @@ public class EchoClient {
 		int send;
 		Socket sock;
 
+		//Constructor
 		public WriteToServer(OutputStream output, Socket sock) throws IOException {
 			this.output = output;
 			this.sock = sock;
 		}
 
+		//Run method is where everything in the thread happens
 		@Override
 		public void run(){
-			try {	// Keep reading until the user stops sending data
+			try {
+				// Keep reading until the user stops sending data
 				while ((send = System.in.read()) != -1){ 
 					output.write(send);
 					output.flush();
 				}
-				sock.shutdownOutput(); // Close connection 
+				sock.shutdownOutput(); // Close connection to the output
 			}
+			//Error Handling
 			catch (IOException ioe){
 				System.out.println("We caught an unexpected exception");
 			}
